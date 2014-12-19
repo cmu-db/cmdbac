01f1137a68ac0cc91abab2a2f82274ca92e1d7b3
Andy Pavlo <pavlo@cs.brown.edu>
2014-12-19 10:35:18 -0500

    Fixed root dir urls

--- db_webcrawler/urls.py
+++ db_webcrawler/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^crawler/', include('crawler.urls')),
+    url(r'', include('crawler.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
--- db_webcrawler/wsgi.py
+++ db_webcrawler/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db_webcrawler.settings")
 
 from django.core.wsgi import get_wsgi_application
