ce1c7217bbda77d3d84ae39e747fe580a4f7c177
zeyuanxy <zeyuanxy@gmail.com>
2015-08-27 18:18:54 -0400

    add scrapy project

---
+++ scrapy/driver/driver/spiders/form.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+import scrapy
+
+class FormSpider(scrapy.Spider):
+    name = "form"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
