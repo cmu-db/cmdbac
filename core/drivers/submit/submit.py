80c865dcf85d2c43b8d5354a1b1a4df6fe7d9aae
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-14 17:15:18 -0400

    use method to choose form

--- core/drivers/submit/submit.py
+++ core/drivers/submit/submit.py
@@ -15,7 +15,9 @@ def get_form_index(br, form):
 		if str(f.attrs.get('action', '')) == form['action']:
 			break
 		if str(f.attrs.get('actions', '')) == form['action']:
-				break
+			break
+		if str(f.attrs.get('method', '')).lower() == form['method']:
+			break
 		index = index + 1
 	return index
 
