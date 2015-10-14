06a09bf4058c73d33859cefd3bc7ddf926905899
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-14 17:31:02 -0400

    minor fix

--- core/drivers/submit/submit.py
+++ core/drivers/submit/submit.py
@@ -12,10 +12,10 @@ import extract
 def get_form_index(br, form):
 	index = 0
         for f in br.forms():
-                if str(f.attrs.get('action', '')) == form['action']:
-			break
-		if str(f.attrs.get('method', '')).lower() == form['method']:
-			break
+            if str(f.attrs.get('action', '')) == form['action']:
+				break
+			if str(f.attrs.get('method', '')).lower() == form['method']:
+				break
 		index = index + 1
 	return index
 
