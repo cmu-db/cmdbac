112540be7bdc3e81e583305801bc2b191e86b1b3
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-17 19:44:08 -0400

    update settings

--- db_webcrawler/settings_example.py
+++ db_webcrawler/settings_example.py
@@ -95,3 +95,11 @@ USE_L10N = True
 STATIC_URL = '/static/'
 
 HTTP_PROXY = ''
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
