e4a2949f1ebddc10ad55f5693c01a5cd72226a4f
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-23 16:03:09 -0400

    show all the queries

--- core/drivers/count/count.py
+++ core/drivers/count/count.py
@@ -4,8 +4,13 @@ def count_query(queries):
 	ret = {}
 	for keyword in keywords:
 		ret[keyword] = 0
+	ret['OTHERS'] = 0
 	for query in queries:
+		counted = False
 		for keyword in keywords:
 			if keyword in query:
 				ret[keyword] += 1
+				counted = True
+		if not counted:
+			ret['OTHERS'] += 1
 	return ret
