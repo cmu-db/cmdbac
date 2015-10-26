f4a72fb67727a58fae7999dd62acc5a2a74afdb4
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-26 16:25:04 -0400

    provide a more readable API for attempt

--- crawler/serializers.py
+++ crawler/serializers.py
@@ -1,7 +1,33 @@
 from models import *
 from rest_framework import serializers
 
+class RepositorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repository
+
+class RuntimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Runtime
+
+class PackageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Package
+
+class DependencySerializer(serializers.ModelSerializer):
+    package_info = PackageSerializer(source='package')
+
+    class Meta:
+        model = Dependency
+        fields = ('id', 'source', 'attempt', 'package_info')
 
 class AttemptSerializer(serializers.ModelSerializer):
+    repo_info = RepositorySerializer(source='repo')
+    runtime_info = RuntimeSerializer(source='runtime')
+    dependencies = DependencySerializer(source='dependency_set', many=True)
+
     class Meta:
         model = Attempt
+        fields = ('id', 'start_time', 'stop_time', 'repo_info', 'sha', 'size', 'log', 'hostname', 
+                  'runtime_info', 'result', 'register', 'login', 'forms_count', 'queries_count', 
+                  'dependencies'
+            )
