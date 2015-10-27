514e34bd2f8b9ae2e48dfb83f49c489143f60679
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-27 00:20:49 -0400

    add two simple prototypes for command line tool

---
+++ tools/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import requests, json
+
+ATTEMPT_URL = "http://127.0.0.1:8000/api/attempt"
+
+def get_attempt_info(attempt_id):
+	pass
+
+def run_benchmark(attempt_id):
+	pass
+
+if __name__ == "__main__":
+	data = json.dumps({'id': '4'})
+	response = requests.get(ATTEMPT_URL, data)
+	print response.json()
