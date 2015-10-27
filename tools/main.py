2924a5e5ab969fd43e62644d5b2844eac549aa87
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-27 14:45:16 -0400

    change prototype

--- tools/main.py
+++ tools/main.py
@@ -6,7 +6,7 @@ ATTEMPT_URL = "http://127.0.0.1:8000/api/attempt"
 def get_attempt_info(attempt_id):
 	pass
 
-def run_benchmark(attempt_id):
+def run_benchmark(attempt_id, database):
 	pass
 
 if __name__ == "__main__":
