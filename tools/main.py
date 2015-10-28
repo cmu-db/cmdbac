fb1ef93dfd671210f18dfc0873c43137bb37c55d
Zeyuan Shang <zeyuanxy@gmail.com>
2015-10-28 19:44:05 -0400

    add args parser for the command line tool

--- tools/main.py
+++ tools/main.py
@@ -10,29 +10,51 @@ django.setup()
 import utils
 ###########
 
+import argparse
 import requests
 import json
 
 ATTEMPT_URL = "http://127.0.0.1:8000/api/attempt"
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_info = subparsers.add_parser('info')
+    parser_info.add_argument("-id", "--id", type=int)
+
+    parser_benchmark = subparsers.add_parser('benchmark')
+    parser_benchmark.add_argument('-repo', '--repo', type=str)
+    parser_benchmark.add_argument('-deploy_id', '--deploy_id', type=int)
+    parser_benchmark.add_argument('-host', '--host', type=str)
+    parser_benchmark.add_argument('-port', '--port', type=int)
+    parser_benchmark.add_argument('-name', '--name', type=str)
+    parser_benchmark.add_argument('-username', '--username', type=str)
+    parser_benchmark.add_argument('-password', '--password', type=str)
+    parser_benchmark.add_argument('-num_threads', '--num_threads', type=int)
+
+    args = parser.parse_args()
+
+    return args
+
+
 def get_attempt_info():
-	data = json.dumps({'id': '4'})
-	response = requests.get(ATTEMPT_URL, data)
-	return response.json()
+    data = json.dumps({'id': '4'})
+    response = requests.get(ATTEMPT_URL, data)
+    return response.json()
 
 def run_benchmark():
-	database = {
-		'host': '127.0.0.1',
-		'port': '3306',
-		'name': 'crawler0',
-		'username': 'root',
-		'password': 'root'
-	}
-	benchmark = {
-		'num_threads': 1
-	}
-	utils.run_benchmark(4, database, benchmark)
-
+    database = {
+        'host': '127.0.0.1',
+        'port': '3306',
+        'name': 'crawler0',
+        'username': 'root',
+        'password': 'root'
+    }
+    benchmark = {
+        'num_threads': 1
+    }
+    utils.run_benchmark(4, database, benchmark)
 
 if __name__ == "__main__":
-	run_benchmark()
+    parse_args()
