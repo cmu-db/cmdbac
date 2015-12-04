55cb7e5fad13b34f6c9c812a5a74016e321d3c07
Zeyuan Shang <zeyuanxy@gmail.com>
2015-12-03 20:52:18 -0500

    update scripts

--- scripts/deploy_repos.py
+++ scripts/deploy_repos.py
@@ -17,7 +17,7 @@ def main():
     total_deployer = int(sys.argv[2])
     database = Database.objects.get(name='MySQL')
 
-    for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
+    for repo in Repository.objects.filter(project_type = 1).filter(latest_attempt__result = 'OK'):
         if repo.id % total_deployer != deploy_id - 1:
             continue
         print 'Attempting to deploy {} using {} ...'.format(repo, repo.project_type.deployer_class)
