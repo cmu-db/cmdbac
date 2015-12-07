5aa7408291ae8aae383c5cded4e02207acea768a
Zeyuan Shang <zeyuanxy@gmail.com>
2015-12-07 02:21:25 -0500

    add queries stats

--- scripts/stats.py
+++ scripts/stats.py
@@ -17,15 +17,33 @@ def tables_stats():
     stats = {}
     for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
         statistics = Statistic.objects.filter(attempt = repo.latest_attempt)
-        if len(statistics) != 0:
-            for s in statistics:
-                stats[s.description] = stats.get(s.description, 0) + s.count
-            num_repos += 1
+        if len(statistics) == 0:
+            continue
+        for s in statistics:
+            stats[s.description] = stats.get(s.description, 0) + s.count
+        num_repos += 1
     print num_repos
     print stats
     avg_stats = {}
     for key, value in stats.iteritems():
         avg_stats[key] = float(value) / num_repos
+    print avg_stats
+
+def queries_stats():
+    num_queries = 0
+    stats = {}
+    for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
+        forms = Form.objects.filter(attempt = repo.latest_attempt)
+        for form in forms:
+            counters = Counter.objects.filter(form = form)
+            for counter in counters:
+                stats[counter.description] = stats.get(counter.description, 0) + counter.count
+                num_queries += counter.count
+    print num_queries
+    print stats
+    avg_stats = {}
+    for key, value in stats.iteritems():
+        avg_stats[key] = float(value * 100) / num_queries
     print avg_stats
 
 def coverage_stats():
@@ -34,23 +52,24 @@ def coverage_stats():
     for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
         forms = Form.objects.filter(attempt = repo.latest_attempt)
         covered_tables = set()
-        if len(forms) != 0:
-            statistics = Statistic.objects.filter(attempt = repo.latest_attempt)
-            if len(statistics) == 0:
-                continue
-            for form in forms:
-                queries = Query.objects.filter(form = form)
-                for query in queries:
-                    m = re.search('FROM `(.*?)`', query.content)
-                    if m:
-                        covered_tables.add(m.group(1))
-            statistic = statistics.get(description = 'num_tables')
-            coverage += float(len(covered_tables) * 100) / statistic.count
-            num_repos += 1
+        if len(forms) == 0:
+            continue
+        statistics = Statistic.objects.filter(attempt = repo.latest_attempt)
+        if len(statistics) == 0:
+            continue
+        for form in forms:
+            queries = Query.objects.filter(form = form)
+            for query in queries:
+                m = re.search('FROM `(.*?)`', query.content)
+                if m:
+                    covered_tables.add(m.group(1))
+        statistic = statistics.get(description = 'num_tables')
+        coverage += float(len(covered_tables) * 100) / statistic.count
+        num_repos += 1
     print coverage / num_repos
 
 def main():
-    coverage_stats()
+    queries_stats()
 
 
 if __name__ == '__main__':
