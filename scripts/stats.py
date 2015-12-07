9140d206fd1d259fe5acbfbc2053b11e43618054
Zeyuan Shang <zeyuanxy@gmail.com>
2015-12-07 01:52:33 -0500

    update scripts

--- scripts/stats.py
+++ scripts/stats.py
@@ -11,9 +11,21 @@ from library.models import *
 import utils
 
 def main():
-    num_
+    num_repos = 0
+    stats = {}
     for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
-        if len(
+        statistic = Statistic.objects.filter(attempt = repo.latest_attempt)
+        if len(statistic) != 0:
+            for s in statistic:
+                stats[s.description] = stats.get(s.description, 0) + s.count
+            num_repos += 1
+    print num_repos
+    print stats
+    avg_stats = {}
+    for key, value in stats.iteritems():
+        avg_stats[key] = float(value) / num_repos
+    print avg_stats
+
 
 if __name__ == '__main__':
     main()
