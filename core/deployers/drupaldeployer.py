b4e039447f88166dd23f0ca1a07c1d33cd512ac5
Zeyuan Shang <zeyuanxy@gmail.com>
2015-12-24 23:10:27 +0800

    add a simple configure_profile in Drupal deployer

--- core/deployers/drupaldeployer.py
+++ core/deployers/drupaldeployer.py
@@ -17,7 +17,7 @@ LOG = logging.getLogger()
 ## =====================================================================
 ## SETTINGS
 ## =====================================================================
-
+WAIT_TIME = 15
 
 ## =====================================================================
 ## Drupal DEPLOYER
@@ -42,8 +42,105 @@ class DrupalDeployer(BaseDeployer):
     ## DEF
 
     def configure_profile(self):
-        pass
-    ## DEF
+        browser = webdriver.PhantomJS()
+        browser.get('http://127.0.0.1:8181/install.php')
+    
+        browser.save_screenshot('/tmp/screenshot.png')
+        # select profile
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.ID, 'edit-profile--4')))
+            browser.find_element_by_id('edit-profile--4').click()
+            browser.find_element_by_tag_name('form').submit()
+            print 'Selecting profile ...'
+        except:
+            pass
+
+        browser.save_screenshot('/tmp/screenshot.png')
+        # select locale
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.TAG_NAME, 'form')))
+            browser.find_element_by_tag_name('form').submit()
+            print 'Selecting locale ...'
+        except:
+            pass
+        
+        browser.save_screenshot('/tmp/screenshot.png')
+        # configure database
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.ID, 'edit-mysql-database')))
+            browser.find_element_by_id('edit-mysql-database').send_keys(self.database_config['name'])
+            browser.find_element_by_id('edit-mysql-username').send_keys(self.database_config['username'])
+            browser.find_element_by_id('edit-mysql-password').send_keys(self.database_config['password'])
+            browser.find_element_by_class_name('fieldset-title').click()
+        except:
+            pass
+        
+        browser.save_screenshot('/tmp/screenshot.png')
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.ID, 'edit-mysql-host')))
+            browser.find_element_by_id('edit-mysql-host').clear()
+            browser.find_element_by_id('edit-mysql-host').send_keys(self.database_config['host'])
+            browser.find_element_by_id('edit-mysql-port').send_keys(self.database_config['port'])
+            browser.find_element_by_tag_name('form').submit()
+            print 'Configuring database ...'
+        except:
+            pass
+
+        browser.save_screenshot('/tmp/screenshot.png')
+        # configure site
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.ID, 'edit-site-name')))
+            browser.find_element_by_id('edit-site-name').send_keys(self.database_config['name'])
+            browser.find_element_by_id('edit-site-mail').send_keys('admin@test.com')
+            browser.find_element_by_id('edit-account-name').send_keys('admin')
+            browser.find_element_by_id('edit-account-pass-pass1').send_keys('admin')
+            browser.find_element_by_id('edit-account-pass-pass2').send_keys('admin')
+            browser.find_element_by_tag_name('form').submit()
+            print 'Configuring site ...'
+        except:
+            pass
+
+        browser.save_screenshot('/tmp/screenshot.png')
+        # feature set
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, 'form-checkbox')))
+            for option in browser.find_elements_by_class_name('form-checkbox'):
+                if not option.is_selected():
+                    option.click()
+            browser.find_element_by_tag_name('form').submit()
+            print 'Configuring feature set ...'
+        except:
+            pass
+
+        browser.save_screenshot('/tmp/screenshot.png')
+        # layout
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.TAG_NAME, 'form')))
+            browser.find_element_by_tag_name('form').submit()
+            print 'Configuring layout ...'
+        except:
+            pass
+
+        browser.save_screenshot('/tmp/screenshot.png')
+        # general user
+        try:
+            WebDriverWait(browser, WAIT_TIME).until(EC.presence_of_element_located((By.ID, 'edit-name')))
+            browser.find_element_by_id('edit-name').send_keys('test')
+            browser.find_element_by_id('edit-mail').send_keys('test@test.com')
+            try:
+                browser.find_element_by_id('edit-pass-pass1').send_keys('test')
+                browser.find_element_by_id('edit-pass-pass2').send_keys('test')
+            except:
+                pass
+            browser.find_element_by_tag_name('form').submit()
+            print 'Configuring general user ...'
+        except:
+            pass
+
+        time.sleep(WAIT_TIME * 2)
+        browser.save_screenshot('/tmp/screenshot.png')
+        browser.quit()
+        ## DEF
 
     def sync_server(self, path):
         LOG.info('Syncing server ...')
