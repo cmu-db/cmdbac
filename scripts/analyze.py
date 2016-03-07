5d694d597459288d40962ee2ab781452a0accbcb
Zeyuan Shang <zeyuanxy@gmail.com>
2016-03-07 23:29:24 +0800

    add todos

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -19,12 +19,27 @@ def analyze_joins():
             queries = Query.objects.filter(action = action)
             for query in queries:
                 content = query.content.upper()
+                has_join = False
                 if 'JOIN' in content:
                     parsed = sqlparse.parse(content)[0]
                     for token in parsed.tokens:
                         if token.is_keyword and 'JOIN' in token.value:
                             result[token.value] = result.get(token.value, 0) + 1
+                            has_join = True
+                if has_join:
+                    for explain in Explain.objects.filter(query = query):
+                        # TODO : do something here to judge the attributes types and statuses
+                        pass
+
     print result
+
+def count_repetitive_queries():
+    result = {}
+    for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
+        for action in Action.objects.filter(attempt = repo.latest_attempt):
+            queries = Query.objects.filter(action = action)
+            for query in queries:
+                # TODO : count the number of repetitive queries per action (queries that are the same and will produce the exact same result)
 
 def main():
     analyze_joins()
