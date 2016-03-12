54e215276a81f63b6ad5a37999472bfbc6a8cf91
Zeyuan Shang <zeyuanxy@gmail.com>
2016-03-12 20:11:16 +0800

    update scripts

--- scripts/deploy_repos.py
+++ scripts/deploy_repos.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "core"))
 
 import time
+import traceback
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmudbac.settings")
 import django
@@ -21,16 +22,18 @@ def main():
     total_deployer = int(sys.argv[3])
     database = Database.objects.get(name=sys.argv[4])
     
-    for repo in Repository.objects.filter(project_type = project_type).filter(latest_attempt = None):
+    for repo in Repository.objects.filter(project_type = project_type).filter(latest_attempt__result = 'OK'):
     # for repo in Repository.objects.filter(project_type = project_type).exclude(Q(latest_attempt__result = 'DE') | Q(latest_attempt__result = 'OK')):
     # for repo in Repository.objects.filter(project_type = 1).filter(latest_attempt__result = 'OK').filter(latest_attempt__log__contains = "[Errno 13] Permission denied: '/var/log/mysql/mysql.log'"):
         if repo.id % total_deployer != deploy_id - 1:
+            continue
+        if not Information.objects.filter(attempt = repo.latest_attempt):
             continue
         print 'Attempting to deploy {} using {} ...'.format(repo, repo.project_type.deployer_class)
         try:
             utils.vagrant_deploy(repo, deploy_id, database)
         except:
-            pass
+            traceback.print_exc()
         finally:
             time.sleep(5)
 
