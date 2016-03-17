91f357b2af9105b7afd2ac99e9542d876371b369
Zeyuan Shang <zeyuanxy@gmail.com>
2016-03-17 23:21:52 +0800

    add analysis : table coverage stats

--- analysis/queries.py
+++ analysis/queries.py
@@ -31,8 +31,39 @@ def query_stats():
 
     print stats
 
+def table_coverage_stats(directory = '.'):
+    stats = []
+
+    for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
+        actions = Action.objects.filter(attempt = repo.latest_attempt)
+        if len(actions) == 0:
+            continue
+
+        statistics = Statistic.objects.filter(attempt = repo.latest_attempt)
+        if len(statistics) == 0:
+            continue
+        statistic = statistics.get(description = 'num_tables')
+        
+        covered_tables = set()
+        for action in actions:
+            for query in Query.objects.filter(action = action):
+                for table in re.findall('FROM\s*\S+', query.content.upper()):
+                    table_name = table.replace('FROM', '').replace("'", "").replace(' ', '')
+                    covered_tables.add(table_name)
+               
+        percentage = int(float(len(covered_tables) * 100) / statistic.count / 10)
+        stats.append(percentage)
+
+    hist, bin_edges = np.histogram(stats, NUM_BINS)
+        
+    with open(os.path.join(directory, 'table_coverage.csv'), 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for i in xrange(NUM_BINS):
+            writer.writerow([int(bin_edges[i]), hist[i]])
+
 def main():
-    query_stats()
+    # query_stats()
+    table_coverage_stats()
 
 if __name__ == '__main__':
     main()
