28f030ba16dc67714f55f7997cd435bf22f75e18
Zeyuan Shang <zeyuanxy@gmail.com>
2016-03-17 23:37:47 +0800

    add analysis : column coverage stats

--- analysis/queries.py
+++ analysis/queries.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "core"))
 import re
 import csv
 import numpy as np
+import sqlparse
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmudbac.settings")
 import django
@@ -39,10 +40,10 @@ def table_coverage_stats(directory = '.'):
         if len(actions) == 0:
             continue
 
-        statistics = Statistic.objects.filter(attempt = repo.latest_attempt)
+        statistics = Statistic.objects.filter(attempt = repo.latest_attempt).filter(description = 'num_tables')
         if len(statistics) == 0:
             continue
-        statistic = statistics.get(description = 'num_tables')
+        table_count = statistics[0].count
         
         covered_tables = set()
         for action in actions:
@@ -51,7 +52,7 @@ def table_coverage_stats(directory = '.'):
                     table_name = table.replace('FROM', '').replace("'", "").replace(' ', '')
                     covered_tables.add(table_name)
                
-        percentage = int(float(len(covered_tables) * 100) / statistic.count / 10)
+        percentage = int(float(len(covered_tables) * 100) / table_count)
         stats.append(percentage)
 
     hist, bin_edges = np.histogram(stats, NUM_BINS)
@@ -61,9 +62,47 @@ def table_coverage_stats(directory = '.'):
         for i in xrange(NUM_BINS):
             writer.writerow([int(bin_edges[i]), hist[i]])
 
+def column_coverage_stats(directory = '.'):
+    stats = []
+
+    for repo in Repository.objects.filter(latest_attempt__result = 'OK'):
+        actions = Action.objects.filter(attempt = repo.latest_attempt)
+        if len(actions) == 0:
+            continue
+
+        informations = Information.objects.filter(attempt = repo.latest_attempt).filter(name = 'columns')
+        if len(informations) == 0:
+            continue
+        information = informations[0]
+        column_count = 0
+        if repo.latest_attempt.database.name == 'PostgreSQL':
+            column_count = len(re.findall('(\(.*?\))[,\]]', information.description))
+        elif repo.latest_attempt.database.name == 'MySQL':
+            column_count = len(re.findall('(\(.*?\))[,\)]', information.description))
+        
+        covered_columns = set()
+        for action in actions:
+            for query in Query.objects.filter(action = action):
+                parsed = sqlparse.parse(query.content)[0]
+                tokens = parsed.tokens
+                for token in tokens:
+                    if isinstance(token, sqlparse.sql.Identifier):
+                        covered_columns.add(token.value)
+               
+        percentage = int(float(len(covered_columns) * 100) / column_count)
+        stats.append(percentage)
+
+    hist, bin_edges = np.histogram(stats, NUM_BINS)
+        
+    with open(os.path.join(directory, 'column_coverage.csv'), 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for i in xrange(NUM_BINS):
+            writer.writerow([int(bin_edges[i]), hist[i]])
+
 def main():
     # query_stats()
-    table_coverage_stats()
+    # table_coverage_stats()
+    column_coverage_stats()
 
 if __name__ == '__main__':
     main()
