012ebbc5d6c31a8d3f92a125f998e6c44c0ea364
Zeyuan Shang <zeyuanxy@gmail.com>
2016-03-22 23:49:13 +0800

    update scripts

--- analysis/dump.py
+++ analysis/dump.py
@@ -2,21 +2,17 @@
 # @Author: Zeyuan Shang
 # @Date:   2016-03-21 01:05:00
 # @Last Modified by:   Zeyuan Shang
-# @Last Modified time: 2016-03-21 01:19:27
+# @Last Modified time: 2016-03-22 23:49:02
 import os
 import csv
-import numpy as np
-
-NUM_BINS = 10
 
 def dump_stats(directory, description, stats):
     if isinstance(stats, list):
-        hist, bin_edges = np.histogram(stats, NUM_BINS)
         with open(os.path.join(directory, description + '.csv'), 'wb') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow([description, 'nums'])
-            for i in xrange(NUM_BINS):
-                writer.writerow([int(bin_edges[i]), hist[i]])
+            writer.writerow([description])
+            for i in stats:
+                writer.writerow([i])
     elif isinstance(stats, dict):
         with open(os.path.join(directory, description + '.csv'), 'wb') as csv_file:
             writer = csv.writer(csv_file)
