02db0c5dc8b2b8d49230f0e8d4c4838f54135c19
Zeyuan Shang <zeyuanxy@gmail.com>
2016-04-10 22:55:44 +0800

    measure columns per table

--- analysis/analyze_table.py
+++ analysis/analyze_table.py
@@ -43,10 +43,12 @@ def column_stats(directory = '.'):
     for repo in Repository.objects.exclude(latest_successful_attempt = None):
         column_informations = Information.objects.filter(attempt = repo.latest_successful_attempt).filter(name = 'columns')
         constraint_informations = Information.objects.filter(attempt = repo.latest_successful_attempt).filter(name = 'constraints')
-        
-        if len(column_informations) > 0 and len(constraint_informations) > 0:
+        num_table_statistics = Statistic.objects.filter(attempt = repo.latest_successful_attempt).filter(description = 'num_tables')
+
+        if len(column_informations) > 0 and len(constraint_informations) > 0 and len(num_table_statistics) > 0:
             column_information = column_informations[0]
             constraint_information = constraint_informations[0]
+            num_tables = num_table_statistics[0].count
 
             project_type_name = repo.project_type.name
             if project_type_name not in stats['column_nullable']:
@@ -65,14 +67,14 @@ def column_stats(directory = '.'):
                 cells = column.split(',')
                 
                 nullable = str(cells[6]).replace("'", "").strip()
-                stats['column_nullable'][project_type_name][nullable] = stats['column_nullable'][project_type_name].get(nullable, 0) + 1
+                stats['column_nullable'][project_type_name][nullable] = stats['column_nullable'][project_type_name].get(nullable, 0) + 1.0 / num_tables
 
                 _type = str(cells[7]).replace("'", "").strip()
-                stats['column_type'][project_type_name][_type] = stats['column_type'][project_type_name].get(_type, 0) + 1
+                stats['column_type'][project_type_name][_type] = stats['column_type'][project_type_name].get(_type, 0) + 1.0 / num_tables
 
                 extra = str(cells[16]).replace("'", "").strip()
                 if extra:
-                    stats['column_extra'][project_type_name][extra] = stats['column_extra'][project_type_name].get(extra, 0) + 1
+                    stats['column_extra'][project_type_name][extra] = stats['column_extra'][project_type_name].get(extra, 0) + 1.0 / num_tables
 
             for column in re.findall(regex, constraint_information.description):
                 cells = column.split(',')
@@ -80,7 +82,7 @@ def column_stats(directory = '.'):
                     constraint_type = str(cells[6]).replace("'", "").strip()
                 elif repo.latest_successful_attempt.database.name == 'MySQL':
                     constraint_type = str(cells[5])[:-1].replace("'", "").strip()
-                stats['column_extra'][project_type_name][constraint_type] = stats['column_extra'][project_type_name].get(constraint_type, 0) + 1
+                stats['column_extra'][project_type_name][constraint_type] = stats['column_extra'][project_type_name].get(constraint_type, 0) + 1.0 / num_tables
 
     dump_all_stats(directory, stats)
 
