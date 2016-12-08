73f60fb35e1d9d9a0c1809db2c35380a9bcb9343
Michael Mior <mmior@uwaterloo.ca>
2016-12-08 11:00:12 -0500

    Add page limit

--- cmudbac/settings_example.py
+++ cmudbac/settings_example.py
@@ -125,7 +125,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 
 # Google Analytics
