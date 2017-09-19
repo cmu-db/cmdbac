50f30b26564eee92dff2ad5baaa5da9bd7c7620a
Zeyuan Shang <zeyuanxy@gmail.com>
2017-09-19 17:59:08 -0400

    update random walk

--- core/drivers/randomdriver.py
+++ core/drivers/randomdriver.py
@@ -4,7 +4,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 import logging
 import requests
 import re
-import copy
 import traceback
 import requests
 import mechanize
@@ -45,40 +44,43 @@ class RandomDriver(BaseDriver):
         main_url = self.deployer.get_main_url()
         for _ in xrange(MAX_RANDOM_WALK_COUNT):
             self.browser.open(main_url)
-            self.random_walk_for_form()
+            self.random_walk_for_form(self.browser)
 
-    def random_walk_for_form(self, depth = MAX_RANDOM_WALK_DEPTH):
+    def random_walk_for_form(self, browser, depth = MAX_RANDOM_WALK_DEPTH):
         if depth == 0:
             return
 
         try:
             last_line_no = self.check_log()
 
-            forms = list(enumerate(list(self.browser.forms())))
-            if len(forms) == 0:
-                return
+            forms = list(enumerate(list(browser.forms())))
+            for idx, form in forms:
+                browser.select_form(nr = idx)
+                form_stats = {
+                    'url': browser.geturl(),
+                    'method': form.method,
+                    'inputs': []
+                }
+                for control in form.controls:
+                    if control.type == 'text':
+                        browser[control.name] = submit.gen_random_value()
+                        form_stats['inputs'].append({
+                            'name': control.name,
+                            'type': control.type
+                        })
+                succ = True
+                try:
+                    browser.submit()
+                except:
+                    succ = False
 
-            idx, form = random.choice(forms)
-            self.browser.select_form(nr = idx)
-            form_stats = {
-                'url': self.browser.geturl(),
-                'method': form.method,
-                'inputs': []
-            }
-            for control in form.controls:
-                if control.type == 'text':
-                    self.browser[control.name] = submit.gen_random_value()
-                    form_stats['inputs'].append({
-                        'name': control.name,
-                        'type': control.type
-                    })
-            self.browser.submit()
+                form_stats['queries'], form_stats['counter'] = self.process_logs(self.check_log(last_line_no), None)
 
-            form_stats['queries'], form_stats['counter'] = self.process_logs(self.check_log(last_line_no), None)
+                if all(not self.equal_form(form_stats, ret_form) for ret_form in self.forms):
+                    self.forms.append(form_stats)
 
-            if all(not self.equal_form(form_stats, ret_form) for ret_form in self.forms):
-                self.forms.append(form_stats)
+                self.random_walk_for_form(browser, depth - 1)
+                if succ:
+                    browser.back()
         except:
             traceback.print_exc()
-
-        self.random_walk_for_form(depth - 1)
