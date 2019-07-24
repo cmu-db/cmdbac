dd39e4a407f952cc4c5a59c50fcf056535d5b8a2
Arthur Pemberton <pembo13@gmail.com>
2019-07-23 21:34:16 -0400

    cleaned up and upgraded crawl_repo.py

--- scripts/crawl_repo.py
+++ scripts/crawl_repo.py
@@ -1,51 +1,65 @@
 #!/usr/bin/env python
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "core"))
+#sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "core"))
 
-import time
-import logging
-logging.basicConfig(filename='repo_crawler.log',level=logging.DEBUG)
-import json
-import traceback
-
+# setup django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmudbac.settings")
 import django
 django.setup()
 
-import crawlers
-from library.models import *
-import utils
+# stdlib imports
+import argparse
+import json
+import logging ; logging.basicConfig(filename='repo_crawler.log',level=logging.DEBUG)
+import time
+import traceback
+# django imports
+from django.conf import settings
+# project imports
+from cmudbac.core import utils
+from cmudbac.library.models import CrawlerStatus
+import cmudbac.core.crawlers
+
 
 def add_module():
-    if len(sys.argv) != 5:
-        return
-    module_name = sys.argv[1]
-    package_name = sys.argv[2]
-    package_type_id = sys.argv[3]
-    package_version = sys.argv[4]
+     # parse command line arguments
+    parser = argparse.ArgumentParser(description='Adds a module to the system.')
+    parser.add_argument('module_name')
+    parser.add_argument('package_name')
+    parser.add_argument('package_type_id')
+    parser.add_argument('package_version')
+    args = parser.parse_args()
+
     try:
-        utils.add_module(module_name, package_name, package_type_id, package_version)
+        utils.add_module(args.module_name, args.package_name, args.package_type_id, args.package_version)
         print('Successfully added new module {}'.format(module_name))
     except:
         print('Failed to add new module {}'.format(repo_name))
         traceback.print_exc()
 
+    return
+
 def add_repository():
-    if len(sys.argv) != 3:
-        return
-    repo_name = sys.argv[1]
-    repo_type_id = sys.argv[2]
+    parser = argparse.ArgumentParser(description='Adds a module to the system.')
+    parser.add_argument('repo_name')
+    parser.add_argument('repo_type_id')
+    args = parser.parse_args()
+
     try:
-        utils.add_repo(repo_name, repo_type_id, None)
+        utils.add_repo(args.repo_name, args.repo_type_id, None)
         print('Successfully added new repository {}'.format(repo_name))
     except:
         print('Failed to add new repository {}'.format(repo_name))
         traceback.print_exc()
+    return
+
 
 def main():
     # add_module()
     add_repository()
+    return
+
 
 if __name__ == '__main__':
     main()
